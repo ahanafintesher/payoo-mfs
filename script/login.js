546ddd28359e16c7ec02e5fe5cac13a815180cb8
ahanafintesher <ahanafintesher01@gmail.com>
2026-04-09 14:28:33 +0600

    pagination fixed attempt - 10

--- script/login.js
+++ script/login.js
@@ -10,7 +10,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
 
         alert('login success');
 
-        window.location.assign('home');
+        window.location.assign('/home.html');
     }
     else{
         alert('login failed');
